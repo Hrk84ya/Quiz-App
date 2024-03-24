f87bf398c932802c5ce59386dce7e3d7bd0c26b3
hrk84ya <hrk84ya@gmail.com>
2024-03-24 11:24:47 +0530

    Basic Quiz App

---
+++ app/src/main/java/com/example/quiz_app/QuestionAnswer.java
@@ -0,0 +1,42 @@
+package com.example.quiz_app;
+
+public class QuestionAnswer {
+    public static String question[]={
+            "How many active S Class Hunters does Korea possess?",
+            "How many days does it take before a gate will break?",
+            "How many people survived the Cartenon Temple (Double Dungeon) slaughter?",
+            "How many times must a Hunter participate in raids before they are eligible to become a guild master?",
+            "What are the ranges of Hunter ranks?",
+            "What classification was Sung Jinwoo given by the System?",
+            "What did the Korean government want to use Essence Stones (Magic Cores) for?",
+            "What is the name of the government organization that oversees the hunters, gates, and guilds in Korea?",
+            "What is the name of the stones commonly found in dungeons that naturally light up the area for Hunters?",
+            "What is the name of Yoo Jinho’s father’s construction company?"
+    };
+
+    public static String[][] choices ={
+            {"7", "20", "5", "10"},
+            {"3", "14", "7", "13"},
+            {"5", "20", "10", "7"},
+            {"10", "20", "5", "15"},
+            {"S->E","A->D","SS->F","S->A"},
+            {"Mage","Necromancer","Assassin","Warrior"},
+            {"Healing Potion","Power Source","Weapons","Comms"},
+            {"Korean Guild Council", "Hunter Regulatory Agency", "Korean Hunters Association", "Gatekeepers Bureau"},
+            {"Luminescence Stones", "Radiant Crystals", "Dungeon Gems", "Illumination Rocks"},
+            {"Jinho Builders","Yoojin Construction", "Sunwoo Enterprises", "Jinwoo Constructors"}
+    };
+
+    public static String[] answers={
+            "7",
+            "7",
+            "7",
+            "20",
+            "S->E",
+            "Necromancer",
+            "Power Source",
+            "Korean Hunters Association",
+            "Luminescence Stones",
+            "Yoojin Construction"
+    };
+}
